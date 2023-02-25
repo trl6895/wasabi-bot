0932bfd462edfb7b9652bbf3265361932a22a1d2
Triston Lincoln <trl6895@g.rit.edu>
2023-02-25 17:22:12 -0500

    added interaction choices on wasabi command

--- commands/wasabing.js
+++ commands/wasabing.js
@@ -3,8 +3,18 @@ const { SlashCommandBuilder } = require("discord.js");
 module.exports = {
   data: new SlashCommandBuilder()
     .setName("wasabing")
-    .setDescription("Responds to being called"),
+    .setDescription("Responds to being called")
+    .addStringOption((option) =>
+      option
+        .setName("interaction")
+        .setDescription("What do you want to say?")
+        .addChoices(
+          { name: "hi!", value: "hello!" },
+          { name: "leaving", value: "bye-bye!" }
+        )
+    ),
   async execute(interaction) {
-    await interaction.reply("yes?");
+    const reply = interaction.options.getString("interaction") ?? "yes?";
+    await interaction.reply(reply);
   },
 };
